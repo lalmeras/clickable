c997b44f42910186da4dabbe653ddb4368dc8252
Laurent Almeras <lalmeras@gmail.com>
2017-09-10 15:41:41 +0200

    Initialization with cookiecutter-pypackage

---
+++ setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'Click >=6.0, <7.0',
+]
+
+setup_requirements = [
+]
+
+test_requirements = [
+]
+
+setup(
+    name='clickable',
+    version='0.0.1',
+    description=("Helper scripts to write click applications development's "
+                 "environment"),
+    long_description=readme + '\n\n' + history,
+    author="Laurent Almeras",
+    author_email='lalmeras@gmail.com',
+    url='https://github.com/lalmeras/clickable',
+    packages=find_packages(include=['clickable']),
+    entry_points={
+        'console_scripts': [
+            'clickable=clickable.cli:main'
+        ]
+    },
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD license",
+    zip_safe=False,
+    keywords='clickable',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+)
