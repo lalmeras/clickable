de31e5b7cc1dfeac7837e08fd84fa05c061ee9f8
Laurent Almeras <lalmeras@gmail.com>
2017-12-27 10:06:32 +0100

    Bump version: 0.0.4dev1 → 0.0.4

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = [
 
 setup(
     name='clickable',
-    version='0.0.4dev1',
+    version='0.0.4',
     description=("Helper scripts to write click applications development's "
                  "environment"),
     long_description=readme + '\n\n' + history,
