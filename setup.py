7819d160ae2554c8647bf6c7208669cca5e2c364
Laurent Almeras <laurent.almeras@kobalt-si.fr>
2018-02-10 16:51:19 +0100

    Bump version: 0.1.1rc1 → 0.1.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ test_requirements = [
 
 setup(
     name='clickable',
-    version='0.1.1rc1',
+    version='0.1.1',
     description=("Helper scripts to write click applications development's "
                  "environment"),
     long_description=readme + '\n\n' + history,
