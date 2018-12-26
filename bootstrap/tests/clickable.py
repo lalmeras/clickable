994bf2940a3290f0963097e38d167db2f3a6bc7a
Laurent Almeras <laurent.almeras@kobalt-si.fr>
2018-12-26 15:59:16 +0100

    remove bootstrap
    bootstrap.py moved to clickable_bootstrap repo

--- bootstrap/tests/clickable.py
+++
@@ -1,3 +0,0 @@
-
-
-clickable_bootstrap_spec = ">= 0.9.0"
--- bootstrap/tests/test_configuration.py
+++
@@ -1,8 +0,0 @@
-
-def test_configuration():
-    import clickable_bootstrap
-    import clickable_logging
-    import logging
-    import os.path
-    clickable_logging.bootstrap(clickable_bootstrap.__name__).log.setLevel(logging.DEBUG)
-    print(clickable_bootstrap._search_configuration(os.path.dirname(__file__)))
